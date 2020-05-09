b53f11c18f422beaea3f80e8a7dc90730b81711b
Mike Moran <mike@houseofmoran.com>
2020-05-09 23:14:47 +0100

    WIP attempt
    currently failing with:
    
    ```
    $ dotnet run
    Unhandled exception. Wasmtime.WasmtimeException: WebAssembly module 'rust_lib' is not valid.
       at Wasmtime.Module..ctor(StoreHandle store, String name, Byte[] bytes)
       at Wasmtime.Host.LoadModule(String name, Byte[] bytes)
       at Wasmtime.Host.LoadModule(String path)
       at Tutorial.Program.Main(String[] args) in /Users/mxm/Code/mine/dotnet-rust-wasm/dotnet-host/Program.cs:line 24
    ```

--- dotnet-host/Program.cs
+++ dotnet-host/Program.cs
@@ -5,13 +5,29 @@ namespace Tutorial
 {
     class Program
     {
+        const int WASI_ERRNO_SUCCESS = 0;
+
         static void Main(string[] args)
         {
             using var host = new Host();
-            using var module = host.LoadModuleText("gcd.wat");
+
+            host.DefineFunction(
+                "wasi_snapshot_preview1",
+                "fd_write",
+                (int fd, int iovs, int iovs_len, int nwritten) =>
+                {
+                    Console.WriteLine("fd_write called");
+                    return WASI_ERRNO_SUCCESS;
+                }
+            );
+
+            using var module = host.LoadModule("../rust-lib/target/wasm32-wasi/release/rust_lib.wasm");
+            Console.WriteLine("Loaded");
 
             using dynamic instance = host.Instantiate(module);
-            Console.WriteLine($"gcd(27, 6) = {instance.gcd(27, 6)}");
+            Console.WriteLine("Instantiated");
+            // Console.WriteLine($"gcd(27, 6) = {instance.gcd(27, 6)}");
+
         }
     }
 }
