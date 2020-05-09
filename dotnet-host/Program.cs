6d02f4bae939e076689bea5a3c0c03f2c8b2815b
Mike Moran <mike@houseofmoran.com>
2020-05-09 22:43:22 +0100

    use example from https://bytecodealliance.github.io/wasmtime/lang-dotnet.html

--- dotnet-host/Program.cs
+++ dotnet-host/Program.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using Wasmtime;
 
-namespace dotnet_host
+namespace Tutorial
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var host = new Host();
+            using var module = host.LoadModuleText("gcd.wat");
+
+            using dynamic instance = host.Instantiate(module);
+            Console.WriteLine($"gcd(27, 6) = {instance.gcd(27, 6)}");
         }
     }
 }
